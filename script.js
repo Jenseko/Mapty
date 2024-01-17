056f949cbb9a0f07cfd7cf3a5d8b5a9e5f233449
Jenseko <jenscode23@gmail.com>
2024-01-17 11:51:22 +0100

    bugfixing distance + duration

--- script.js
+++ script.js
@@ -75,8 +75,8 @@ class Cycling extends Workout {
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
+const inputDuration = document.querySelector('.form__input--distance');
+const inputDistance = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
@@ -135,7 +135,7 @@ class App {
   _showForm(mapE) {
     this.#mapEvent = mapE;
     form.classList.remove('hidden');
-    inputDistance.focus();
+    inputDuration.focus();
   }
 
   _hideForm() {
